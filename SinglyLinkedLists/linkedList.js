41dc0002d5379a4c3aa57a6abb4414c88e4c4b5c
rengebre <russell.engebretson@gmail.com>
2021-12-15 11:06:27 -0800

    fully implement singly linked list with all methods

--- SinglyLinkedLists/linkedList.js
+++ SinglyLinkedLists/linkedList.js
@@ -64,12 +64,96 @@ class SinglyLinkedList {
     this.length += 1;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return;
+    let node = this.head;
+    let count = 0;
+    while (count !== index) {
+      count++;
+      node = node.next;
+    }
+    return node;
+  }
+
+  set(index, val) {
+    let node = this.get(index);
+    if (!node) return;
+    node.val = val;
+    return node;
+  }
+
+  insert(index, val) {
+    if (index > this.length || index < 0) return;
+    else if (index === 0) this.unshift(val);
+    else if (index === this.length) this.push(val);
+    else {
+      let newNode = new Node(val);
+      let prev = this.get(index - 1);
+      let current = prev.next;
+
+      prev.next = newNode;
+      newNode.next = current;
+      this.length += 1;
+    }
+    return this;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length - 1) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let prev = this.get(index - 1);
+    let removed = prev.next;
+
+    prev.next = removed.next;
+    // curr.next = null;
+    this.length -= 1;
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    let prev = null;
+    this.head = this.tail;
+    this.tail = node;
+
+    while (node) {
+      let temp = node.next;
+      node.next = prev;
+      prev = node;
+      node = temp;
+    }
+
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
-console.log(list.unshift("Hello"));
-console.log(list.unshift("Pre Hello"));
-// list.push("Hello");
+list.unshift("Hello");
+list.unshift("Pre Hello");
+list.insert(2, 8);
+list.insert(0, 'unshiiiiffttttt');
+list.insert(2, "middle");
+
+console.log(list.reverse());
+console.log("******** list ********")
+let node = list.head;
+while (node) {
+  console.log(node)
+  node = node.next;
+}
+
+console.log("******** reversed ********")
+list.reverse();
+node = list.head;
+while (node) {
+  console.log(node)
+  node = node.next;
+}
+
+
 // list.push("there");
 // console.log(list);
 // console.log("SHIFT", list.shift())
