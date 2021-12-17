1a7debc4f00db6c738b4c3fe6d6ef165b9253758
rengebre <russell.engebretson@gmail.com>
2021-12-16 21:15:47 -0800

    add queue implementation

---
+++ StacksAndQueues/queue.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+    } else {
+      this.last.next = newNode;
+    }
+
+    this.last = newNode;
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (this.size === 0) return null;
+    let popped = this.first;
+    if (this.size === 1) {
+      this.last = null;
+    }
+
+    this.first = popped.next;
+    this.size--;
+    return popped.value;
+  }
+}
+
+let queue = new Queue();
+queue.enqueue("First");
+console.log(queue)
+queue.enqueue("Second");
+console.log(queue)
+queue.enqueue("Third");
+console.log(queue)
+
+queue.dequeue()
+console.log(queue)
+queue.dequeue()
+console.log(queue)
+queue.dequeue()
+console.log(queue)
